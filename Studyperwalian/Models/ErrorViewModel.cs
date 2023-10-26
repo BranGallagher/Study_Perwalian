b2c375c48bc559fb607f379e07792cd3e151215e
Branda <135972024+BranGallagher@users.noreply.github.com>
2023-10-26 08:59:25 +0700

    Add files via upload

---
+++ Studyperwalian/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Studyperwalian.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
